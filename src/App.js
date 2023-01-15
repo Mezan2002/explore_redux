644e99649ce8082166845cd58873f3604f60a520
Mezan2002 <mezanurr009@gmail.com>
2023-01-15 20:18:12 +0600

    counter by redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useReducer } from "react";
 import "./App.css";
 
 function App() {
+  const initialState = 0;
+  const reducerFn = (state, action) => {
+    if (action.type === "INC") {
+      return state + 1;
+    } else if (action.type === "DEC") {
+      return state - 1;
+    } else {
+      return state;
+    }
+  };
+  const [state, dispatch] = useReducer(reducerFn, initialState);
   return (
     <div className="App">
       <div>
-        <h2>0</h2>
-        <button>-</button>
-        <button>+</button>
+        <h2>{state}</h2>
+        <button onClick={() => dispatch({ type: "DEC" })}>-</button>
+        <button onClick={() => dispatch({ type: "INC" })}>+</button>
       </div>
     </div>
   );
